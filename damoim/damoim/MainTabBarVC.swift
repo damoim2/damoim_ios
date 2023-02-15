405bab116584c453179d083322689c0958950f95
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2023-02-15 20:16:12 +0900

    [Add]View/UI 1,2,3 & Feature 1 완료

---
+++ damoim/damoim/MainTabBarVC.swift
@@ -0,0 +1,39 @@
+//
+//  MainTabBarVC.swift
+//  damoim
+//
+//  Created by 원동진 on 2023/02/15.
+//
+
+import UIKit
+class MainTabBarVC : UITabBarController {
+    let firstNC = UINavigationController.init(rootViewController: HomeVC())
+    let secondNC = UINavigationController.init(rootViewController: CalendarVC())
+    let thirdNC = UINavigationController.init(rootViewController: ChatVC())
+    let fourthNC = UINavigationController.init(rootViewController: MyProfileVC())
+    let firstTabBarItem = UITabBarItem(title: "홈", image: UIImage(named: "home"), tag: 0)
+    let secondTabBarItem = UITabBarItem(title: "달력", image: UIImage(named: "calendar"), tag: 1)
+    let thirdTabBarItem = UITabBarItem(title: "채팅", image: UIImage(named: "chat"), tag: 2)
+    let fourthTabBarItem = UITabBarItem(title: "내 프로필", image: UIImage(named: "myprofile"), tag: 3)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        setNCinit()
+        self.tabBar.tintColor = UIColor(named: "purple01")
+
+    }
+
+}
+extension MainTabBarVC {
+    func setNCinit(){
+        self.viewControllers = [firstNC,secondNC,thirdNC,fourthNC]
+        firstTabBarItem.selectedImage = UIImage(named: "fillhome")
+        firstNC.tabBarItem = firstTabBarItem
+        secondTabBarItem.selectedImage = UIImage(named: "fillcalendar")
+        secondNC.tabBarItem = secondTabBarItem
+        thirdTabBarItem.selectedImage = UIImage(named: "fillchat")
+        thirdNC.tabBarItem = thirdTabBarItem
+        fourthTabBarItem.selectedImage = UIImage(named: "fillmyprofile")
+        fourthNC.tabBarItem = fourthTabBarItem
+    }
+}
---
+++ damoim/damoim/MyProfile/MyProfileVC.swift
@@ -0,0 +1,19 @@
+//
+//  MyProfileVC.swift
+//  damoim
+//
+//  Created by 원동진 on 2023/02/15.
+//
+
+import Foundation
+import UIKit
+class MyProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+
+}
