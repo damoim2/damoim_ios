9e7fa19555059cb4f2c60339a412d82f228d2e41
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2023-02-21 00:07:14 +0900

    [Add]:View/UI 1,2,3 구현

--- damoim/damoim/Home/HomeVC.swift
+++ damoim/damoim/Home/HomeVC.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
-
+import SnapKit
 class HomeVC: UIViewController {
-
+    var testView : UIView = {
+        let test  = UIView()
+        return test
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setAddView()
+        setAutoLayout()
+    
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    private func setAddView(){
+        view.addSubview(testView)
+    }
+    private func setAutoLayout(){
+        testView.snp.makeConstraints { make in
+            make.left.right.top.bottom.equalTo(view.safeAreaLayoutGuide)
+            
+            
+        }
+    }
 
 }
 
