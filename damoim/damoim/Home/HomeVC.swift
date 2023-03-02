fe056db7e379b1a2cffe7ed54547e473512af273
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2023-03-02 22:19:44 +0900

    feature-issue# 1: [Add]View/UI 8 일정 추가 구현

--- damoim/damoim/Home/HomeVC.swift
+++ damoim/damoim/Home/HomeVC.swift
@@ -12,7 +12,7 @@ struct ExampleCVItem{
     let groupName : String?
 }
 class HomeVC: UIViewController {
-    let sample = (1...10).map{_ in return ExampleCVItem(groupImage: UIImage(named: "testImg"), groupName: "test")}
+    let sample = (1...2).map{_ in return ExampleCVItem(groupImage: UIImage(named: "testImg"), groupName: "test")}
     static var numOfcol = 2
     var gridFlowLayout : GridCollectionVFL = {
         let layout = GridCollectionVFL()
@@ -29,8 +29,8 @@ class HomeVC: UIViewController {
         grouptList.contentInset = .zero
         grouptList.clipsToBounds = true
         grouptList.register(GroupCVCell.self, forCellWithReuseIdentifier: GroupCVCell.identi)
+        grouptList.register(AddGroupCVCell.self, forCellWithReuseIdentifier: AddGroupCVCell.identi)
         grouptList.register(GroupCVCellHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: GroupCVCellHeader.identi)
-        
         return grouptList
     }()
     override func viewDidLoad() {
@@ -60,27 +60,48 @@ extension HomeVC {
     }
 }
 extension HomeVC : UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.sample.count
+        if section == 0{
+            return self.sample.count
+        }else{
+            return 1
+        }
+        
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let groupCell = collectionView.dequeueReusableCell(withReuseIdentifier: GroupCVCell.identi, for: indexPath) as! GroupCVCell
-        groupCell.groupImgView.layer.cornerRadius = 16
-        groupCell.groupImgView.layer.masksToBounds = true
-        groupCell.prepare(model: self.sample[indexPath.item])
-        return groupCell
+        
+        if indexPath.section == 0{
+            let groupCell = collectionView.dequeueReusableCell(withReuseIdentifier: GroupCVCell.identi, for: indexPath) as! GroupCVCell
+            groupCell.groupImgView.layer.cornerRadius = 16
+            groupCell.groupImgView.layer.masksToBounds = true
+            groupCell.prepare(model: self.sample[indexPath.item])
+            return groupCell
+        }else {
+            let addGroupCell = collectionView.dequeueReusableCell(withReuseIdentifier: AddGroupCVCell.identi, for: indexPath) as! AddGroupCVCell
+            return addGroupCell
+        }
+        
     }
-//MARK: - Header
+    //MARK: - Header & footer
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+       
         if kind == UICollectionView.elementKindSectionHeader{
+            if indexPath.section == 0{
+                
+            }
             guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: GroupCVCellHeader.identi, for: indexPath) as? GroupCVCellHeader else {return UICollectionReusableView()}
             header.gridTwoAction = { [unowned self] in
                 gridFlowLayout.numberOfColumns = 2
                 groupListCV.reloadData()
+                
             }
             header.gridThreeAction = { [unowned self] in
                 gridFlowLayout.numberOfColumns = 3
                 groupListCV.reloadData()
+                
             }
             
             return header
@@ -88,9 +109,14 @@ extension HomeVC : UICollectionViewDataSource {
         return UICollectionReusableView()
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 0{
             let width: CGFloat = collectionView.frame.width
             let height: CGFloat = 50
             return CGSize(width: width, height: height)
+        }else {
+            return CGSize(width: 0, height: 0)
+        }
+        
     }
     
 }
@@ -98,13 +124,12 @@ extension HomeVC : UICollectionViewDataSource {
 extension HomeVC : UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         guard let flowLayout = collectionViewLayout as? GridCollectionVFL,
-             flowLayout.numberOfColumns > 0
-           else { fatalError() }
-           
-           let widthOfCells = collectionView.bounds.width - (collectionView.contentInset.left + collectionView.contentInset.right)
+              flowLayout.numberOfColumns > 0
+        else { fatalError() }
+        
+        let widthOfCells = collectionView.bounds.width - (collectionView.contentInset.left + collectionView.contentInset.right)
         let widthOfSpacing =  CGFloat(flowLayout.numberOfColumns - 1) * flowLayout.cellSpacing
-           let width = (widthOfCells - widthOfSpacing) / CGFloat(flowLayout.numberOfColumns)
-
+        let width = (widthOfCells - widthOfSpacing) / CGFloat(flowLayout.numberOfColumns)
         return CGSize(width: width, height: width * flowLayout.ratioHeightToWidth)
     }
 }
