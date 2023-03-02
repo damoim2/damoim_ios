5b8764bb5fd231df0b99340f06ed77d6fdcbec95
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2023-03-02 22:25:47 +0900

    feature-issue #1: [Etx]이슈 이름 잘못기재 변경함

---
+++ damoim/damoim/Home/ScheduleView.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleView.swift
+//  damoim
+//
+//  Created by 원동진 on 2023/03/02.
+//
+
+import UIKit
+
+class ScheduleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
