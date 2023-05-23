c423433129cf803a684b0ac695b7a85bd0bc134c
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2023-05-23 15:35:37 +0900

    #1 : [temp] 글상세화면 언급 text강조 구현중

--- damoim/damoim/Home/TextDeatil/TextDetailVC.swift
+++ damoim/damoim/Home/TextDeatil/TextDetailVC.swift
@@ -14,16 +14,16 @@ struct CommentDataStruct {
     let comment : String
 }
 class TextDetailVC: UIViewController, ConstraintRelatableTarget {
+    var sampleCommentData = [CommentDataStruct(userImg: UIImage(named: "testImg")!, userName: "김초코", date: "00월 00일", comment: "능히 영락과 우리의 것이다. 뛰노는 그들에게 고동을 따뜻한 목숨이 있다. 장식하는 청춘의 끓는 고동을 물방아 동산에는 생명을 있는가? 인간이 얼마나 목숨을 약동하다. 동력은 것은 피고, 주는 살 있다. 길을 들어 곧 그리하였는가?"),CommentDataStruct(userImg: UIImage(named: "testImg")!, userName: "김코난", date: "00월 00일", comment: "능히 영락과 우리의 것이다. 뛰노는 그들에게 고동을 따뜻한 목숨이 있다. 장식하는 청춘의 끓는 고동을 물방아 동산에는 생명을 있는가? 인간이 얼마나 목숨을 약동하다. 동력은 것은 피고, 주는 살 있다. 길을 들어 곧 그리하였는가?"),CommentDataStruct(userImg: UIImage(named: "testImg")!, userName: "갱갱", date: "00월 00일", comment: "능히 영락과 우리의 것이다. 뛰노는 그들에게 고동을 따뜻한 목숨이 있다. 장식하는 청춘의 끓는 고동을 물방아 동산에는 생명을 있는가? 인간이 얼마나 목숨을 약동하다. 동력은 것은 피고, 주는 살 있다. 길을 들어 곧 그리하였는가?")
+    ]
+
     private lazy var containerView : UIView = {
         let view = UIView()
         return view
     }()
     // Commet 관련
     var commentMaxHeight = 0
-    let textViewPlaceHoler = "댓글을 입력해주세요."// 댓글 textView placeHolder
-
-    var sampleCommentData = [CommentDataStruct(userImg: UIImage(named: "testImg")!, userName: "김초코", date: "00월 00일", comment: "능히 영락과 우리의 것이다. 뛰노는 그들에게 고동을 따뜻한 목숨이 있다. 장식하는 청춘의 끓는 고동을 물방아 동산에는 생명을 있는가? 인간이 얼마나 목숨을 약동하다. 동력은 것은 피고, 주는 살 있다. 길을 들어 곧 그리하였는가?"),CommentDataStruct(userImg: UIImage(named: "testImg")!, userName: "김코난", date: "00월 00일", comment: "능히 영락과 우리의 것이다. 뛰노는 그들에게 고동을 따뜻한 목숨이 있다. 장식하는 청춘의 끓는 고동을 물방아 동산에는 생명을 있는가? 인간이 얼마나 목숨을 약동하다. 동력은 것은 피고, 주는 살 있다. 길을 들어 곧 그리하였는가?"),CommentDataStruct(userImg: UIImage(named: "testImg")!, userName: "갱갱", date: "00월 00일", comment: "능히 영락과 우리의 것이다. 뛰노는 그들에게 고동을 따뜻한 목숨이 있다. 장식하는 청춘의 끓는 고동을 물방아 동산에는 생명을 있는가? 인간이 얼마나 목숨을 약동하다. 동력은 것은 피고, 주는 살 있다. 길을 들어 곧 그리하였는가?")
-    ]
+    let textViewPlaceHoler = "@김초코 댓글을 입력해주세요."// 댓글 textView placeHolder
     
     var groupName : String = "모임 이름"
     private lazy var commentTableView : UITableView = {
@@ -44,9 +44,12 @@ class TextDetailVC: UIViewController, ConstraintRelatableTarget {
     private lazy var commentTextView : UITextView = {
         let textView = UITextView()
         textView.isScrollEnabled = false
-        textView.text = textViewPlaceHoler
         textView.font = UIFont(name: CustomFont.Regular.rawValue, size: 13)
         textView.textColor = UIColor(named: "grey03")
+        let attributesString = NSMutableAttributedString(string: textViewPlaceHoler)
+        attributesString.addAttribute(.foregroundColor, value: UIColor(named: "purple01") as Any, range: (textViewPlaceHoler as NSString).range(of: "@김초코"))
+        attributesString.addAttribute(.backgroundColor, value: UIColor(named: "purple04") as Any, range: (textViewPlaceHoler as NSString).range(of: "@김초코"))
+        textView.attributedText = attributesString
         textView.textContainerInset = .init(top: 10, left: 10, bottom: 10, right: 10)
         textView.backgroundColor = UIColor(named: "grey05")
         textView.layer.cornerRadius = 16
@@ -87,12 +90,22 @@ extension TextDetailVC : UITableViewDelegate,UITableViewDataSource{
         commentTVC.setComment(model: sampleCommentData[indexPath.row])
         return commentTVC
     }
+//    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UITableView
+//    }
+    //삭제
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            
+        }
+    }
 }
 extension TextDetailVC  {
     private func addSubView(){
         self.view.addSubview(containerView)
         containerView.addSubview(commentTableView)
         containerView.addSubview(commentTextView)
+
     }
     private func setAutoLayout(){
         containerView.snp.makeConstraints { make in
